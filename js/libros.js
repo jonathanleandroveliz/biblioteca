69755dc40f10e3f592457b99725a90638b5801ea
jonathanleandroveliz <jonathanleandroveliz@gmail.com>
2023-11-11 14:03:39 -0300

    modificando la forma de mostrar libros en la vista

--- js/libros.js
+++ js/libros.js
@@ -1,7 +1,40 @@
 var aplic = angular.module('buscarLibro', []);
 aplic.controller('buscarCtrl', function($scope,$http){
 
-    $scope.getBooks = function(){
+    $scope.books = [];
+
+    $scope.getBooks = function () {
+      $http.get("http://openlibrary.org/search.json?q=" + $scope.searchTerm)
+        .then(function (response) {
+            $scope.books = [];
+            $scope.getBooks = function () {
+              $http.get("http://openlibrary.org/search.json?q=" + $scope.searchTerm)
+                .then(function (response) {
+                  $scope.books = [];
+    
+                  if (!response.data.docs.length) {
+                    $scope.books.push({
+                      title: 'No se encontraron libros',
+                      author_name: [''],
+                      first_publish_year: ''
+                    });
+                  } else {
+                    for (var i = 0; i < Math.min(response.data.docs.length, 10); i++) {
+                      $scope.books.push({
+                        title: response.data.docs[i].title,
+                        author_name: response.data.docs[i].author_name,
+                        first_publish_year: response.data.docs[i].first_publish_year,
+                        isbn: response.data.docs[i].isbn
+                      });
+                    }
+                  }
+                });
+            };
+    
+        });
+    };
+
+   /* $scope.getBooks = function(){
         document.getElementById('salida').innerHTML="";
         $http.get("http://openlibrary.org/search.json?q="
         +document.getElementById("entrada").value)
@@ -23,7 +56,7 @@ aplic.controller('buscarCtrl', function($scope,$http){
             }
             
         })
-    }
+    }*/
 });
 
 
